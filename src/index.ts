85162f73948e5815b2cba119b5543729106366be
hsjoihs <hirotaka_satou@dwango.co.jp>
2023-10-23 16:01:56 +0900

    関数が長すぎるので、とりあえず機能ごとに分割

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,84 @@ export function inspect_codeblock(textbook_filepath: string, config: { src: stri
   });
   return all_success;
 }
-export function inspect_codeblock_and_return_message(textbook_filepath: string, config: { src: string }): { success: Boolean, message: string, additionally?: unknown }[] {
+
+type TestRes = { success: Boolean, message: string, additionally?: unknown };
+
+function handle_exact(textbook_filepath: string, command_args: string[], matched_file_content: string, src_folder: string): TestRes {
+  const sample_file_name = command_args[1];
+  const sample_file_path = src_folder + sample_file_name;
+  const sample_file_content = fs.readFileSync(sample_file_path, { encoding: "utf-8" }).replace(/\r?\n/g, "\n");
+  if (matched_file_content !== sample_file_content) {
+    return {
+      success: false,
+      message: ` MISMATCH FOUND 
+in ${textbook_filepath}
+with the code block labeled ${sample_file_name}
+Please compare the textbook with the content of ${sample_file_path} `
+    };
+  } else {
+    return {
+      success: true,
+      message:
+        ` OK: "${textbook_filepath}" のコードブロック "exact ${sample_file_name}" は "${sample_file_path}" と一致しています`
+    };
+  }
+}
+
+function handle_diff(textbook_filepath: string, command_args: string[], matched_file_content: string, src_folder: string): TestRes {
+  const old_sample_file_name = command_args[1];
+  const new_sample_file_name = command_args[2];
+  const old_sample_file_path = src_folder + old_sample_file_name;
+  const new_sample_file_path = src_folder + new_sample_file_name;
+  const actual_diff = getDiffFromFileNames(old_sample_file_path, new_sample_file_path);
+  // 空行に対しての trailing space でテストが落ちるのはしょうもないので、あらかじめ両者から削っておく
+  if (trimEndOnAllLines(matched_file_content) !== trimEndOnAllLines(actual_diff)) {
+    return {
+      success: false, message: ` MISMATCH FOUND 
+in ${textbook_filepath}
+with the code block labeled "diff ${old_sample_file_name} ${new_sample_file_name}"
+The diff of ${old_sample_file_path} with ${new_sample_file_path} is as follows: \n\`\`\`\n${actual_diff}\`\`\` 
+But the content in the textbook is as follows: \n\`\`\`\n${matched_file_content}\`\`\` 
+`};
+  } else {
+    return {
+      success: true,
+      message: ` OK: "${textbook_filepath}" のコードブロック "diff ${old_sample_file_name} ${new_sample_file_name}" は "${old_sample_file_path}" と "${new_sample_file_path}" の diff と一致しています`
+    };
+  }
+}
+
+function handle_partial(textbook_filepath: string, command_args: string[], matched_file_content: string, src_folder: string): TestRes {
+  const starting_line_num = Number(command_args[2]);
+  const sample_file_name = command_args[1];
+  const sample_file_path = src_folder + sample_file_name;
+  const sample_file_content = fs.readFileSync(sample_file_path, { encoding: "utf-8" }).replace(/\r?\n/g, "\n");
+
+  // 末尾の改行を削って行数を数える
+  const 行数 = matched_file_content.trimEnd().split("\n").length;
+
+  // 1-index で与えられているので、実際に配列アクセスするときには 1 を引いておく必要がある
+  const partial_content = sample_file_content.split("\n").slice(starting_line_num - 1, starting_line_num - 1 + 行数).join("\n");
+
+  // 空行に対しての trailing space でテストが落ちるのはしょうもないので、あらかじめ両者から削っておく
+  if (trimEndOnAllLines(partial_content.trimEnd()) !== trimEndOnAllLines(matched_file_content.trimEnd())) {
+    return {
+      success: false,
+      message: ` MISMATCH FOUND 
+          in ${textbook_filepath}
+          with the code block labeled "partial ${sample_file_name} ${starting_line_num}"
+         Please compare the textbook with the content of ${sample_file_path} `,
+      additionally: { partial_content, matched_file_content },
+    }
+  } else {
+    return {
+      success: true,
+      message: ` OK: "${textbook_filepath}" のコードブロック "partial ${sample_file_name} ${starting_line_num}" は "${sample_file_path}" の ${starting_line_num} 行目からの ${行数} 行と一致しています`
+    };
+  }
+}
+
+export function inspect_codeblock_and_return_message(textbook_filepath: string, config: { src: string }): TestRes[] {
   const textbook_content = fs.readFileSync(textbook_filepath, { encoding: "utf-8" }).replace(/\r?\n/g, "\n");
   const match = [...textbook_content.matchAll(/<!--\s*assert[-_]codeblock\s+(.*?)-->[\n\s]*```(.*?)\n([\s\S]*?)```/gm)]
   if (!match.length) return [];
@@ -46,73 +123,11 @@ export function inspect_codeblock_and_return_message(textbook_filepath: string, 
     ([command, file_type, matched_file_content]) => {
       const command_args = command.trim().split(/\s+/);
       if (command_args[0] === "exact") {
-        const sample_file_name = command_args[1];
-        const sample_file_path = config.src + sample_file_name;
-        const sample_file_content = fs.readFileSync(sample_file_path, { encoding: "utf-8" }).replace(/\r?\n/g, "\n");
-        if (matched_file_content !== sample_file_content) {
-          return {
-            success: false,
-            message: ` MISMATCH FOUND 
- in ${textbook_filepath}
- with the code block labeled ${sample_file_name}
-Please compare the textbook with the content of ${sample_file_path} `
-          };
-        } else {
-          return {
-            success: true,
-            message:
-              ` OK: "${textbook_filepath}" のコードブロック "exact ${sample_file_name}" は "${sample_file_path}" と一致しています`
-          };
-        }
+        return handle_exact(textbook_filepath, command_args, matched_file_content, config.src);
       } else if (command_args[0] === "diff") {
-        const old_sample_file_name = command_args[1];
-        const new_sample_file_name = command_args[2];
-        const old_sample_file_path = config.src + old_sample_file_name;
-        const new_sample_file_path = config.src + new_sample_file_name;
-        const actual_diff = getDiffFromFileNames(old_sample_file_path, new_sample_file_path);
-        // 空行に対しての trailing space でテストが落ちるのはしょうもないので、あらかじめ両者から削っておく
-        if (trimEndOnAllLines(matched_file_content) !== trimEndOnAllLines(actual_diff)) {
-          return {
-            success: false, message: ` MISMATCH FOUND 
- in ${textbook_filepath}
- with the code block labeled "diff ${old_sample_file_name} ${new_sample_file_name}"
- The diff of ${old_sample_file_path} with ${new_sample_file_path} is as follows: \n\`\`\`\n${actual_diff}\`\`\` 
- But the content in the textbook is as follows: \n\`\`\`\n${matched_file_content}\`\`\` 
- `};
-        } else {
-          return {
-            success: true,
-            message: ` OK: "${textbook_filepath}" のコードブロック "diff ${old_sample_file_name} ${new_sample_file_name}" は "${old_sample_file_path}" と "${new_sample_file_path}" の diff と一致しています`
-          };
-        }
+        return handle_diff(textbook_filepath, command_args, matched_file_content, config.src);
       } else if (command_args[0] === "partial") {
-        const starting_line_num = Number(command_args[2]);
-        const sample_file_name = command_args[1];
-        const sample_file_path = config.src + sample_file_name;
-        const sample_file_content = fs.readFileSync(sample_file_path, { encoding: "utf-8" }).replace(/\r?\n/g, "\n");
-
-        // 末尾の改行を削って行数を数える
-        const 行数 = matched_file_content.trimEnd().split("\n").length;
-
-        // 1-index で与えられているので、実際に配列アクセスするときには 1 を引いておく必要がある
-        const partial_content = sample_file_content.split("\n").slice(starting_line_num - 1, starting_line_num - 1 + 行数).join("\n");
-
-        // 空行に対しての trailing space でテストが落ちるのはしょうもないので、あらかじめ両者から削っておく
-        if (trimEndOnAllLines(partial_content.trimEnd()) !== trimEndOnAllLines(matched_file_content.trimEnd())) {
-          return {
-            success: false,
-            message: ` MISMATCH FOUND 
-                in ${textbook_filepath}
-                with the code block labeled "partial ${sample_file_name} ${starting_line_num}"
-               Please compare the textbook with the content of ${sample_file_path} `,
-            additionally: { partial_content, matched_file_content },
-          }
-        } else {
-          return {
-            success: true,
-            message: ` OK: "${textbook_filepath}" のコードブロック "partial ${sample_file_name} ${starting_line_num}" は "${sample_file_path}" の ${starting_line_num} 行目からの ${行数} 行と一致しています`
-          };
-        }
+        return handle_partial(textbook_filepath, command_args, matched_file_content, config.src);
       } else {
         return {
           success: false,
