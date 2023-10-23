39fe79620ceceeeb2d7b5bb33b3636f2be1fb24d
hsjoihs <hirotaka_satou@dwango.co.jp>
2023-10-23 17:06:55 +0900

    コマンド内でファイル名がミスっているとき専用のメッセージを導入

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import fs from "fs";
 import { structuredPatch } from 'diff';
 
-function getDiffFromFileNames(oldFileName: string, newFileName: string) {
-  const oldStr = fs.readFileSync(oldFileName, { encoding: "utf-8" });
-  const newStr = fs.readFileSync(newFileName, { encoding: "utf-8" });
+class WrongFileNameInCommandError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "WrongFileNameInCommandError";
+  }
+}
+
+function readFileSync(file_name: string, context: string): string {
+  if (!fs.existsSync(file_name)) {
+    throw new WrongFileNameInCommandError(` FILE NOT FOUND 
+Cannot find a file "${file_name}" mentioned in the code block labeled "${context}" `);
+  }
+  return fs.readFileSync(file_name, { encoding: "utf-8" });
+}
+
+function getDiffFromFileNames(oldFileName: string, newFileName: string, context: string) {
+  const oldStr = readFileSync(oldFileName, context);
+  const newStr = readFileSync(newFileName, context);
   const diff = structuredPatch(oldFileName, newFileName, oldStr, newStr, "", "", { context: 1e6 });
   const ret: string[] = [];
   for (let i = 0; i < diff.hunks.length; i++) {
@@ -19,8 +34,8 @@ function trimEndOnAllLines(str: string) {
 
 export function inspect_codeblock(textbook_filepath: string, config: { src: string }): boolean {
   let all_success = true;
-  inspect_codeblock_and_return_message(textbook_filepath, config).forEach(({ success, message, additionally }) => {
-    if (success) {
+  inspect_codeblock_and_return_message(textbook_filepath, config).forEach(({ is_success, message, additionally }) => {
+    if (is_success) {
       console.log(`\x1b[32m${message}\x1b[0m`);
     } else {
       console.log(`\x1b[33m${message}\x1b[0m`);
@@ -30,22 +45,22 @@ export function inspect_codeblock(textbook_filepath: string, config: { src: stri
       console.log(additionally);
     }
 
-    if (!success) {
+    if (!is_success) {
       all_success = false;
     }
   });
   return all_success;
 }
 
-type TestRes = { success: Boolean, message: string, additionally?: unknown };
+type TestRes = { is_success: Boolean, message: string, additionally?: unknown };
 
 function handle_exact(textbook_filepath: string, command_args: string[], matched_file_content: string, src_folder: string): TestRes {
   const sample_file_name = command_args[1];
   const sample_file_path = src_folder + sample_file_name;
-  const sample_file_content = fs.readFileSync(sample_file_path, { encoding: "utf-8" }).replace(/\r?\n/g, "\n");
+  const sample_file_content = readFileSync(sample_file_path, command_args.join(" ")).replace(/\r?\n/g, "\n");
   if (matched_file_content !== sample_file_content) {
     return {
-      success: false,
+      is_success: false,
       message: ` MISMATCH FOUND 
 in ${textbook_filepath}
 with the code block labeled ${sample_file_name}
@@ -53,7 +68,7 @@ Please compare the textbook with the content of ${sample_file_path} `
     };
   } else {
     return {
-      success: true,
+      is_success: true,
       message:
         ` OK: "${textbook_filepath}" のコードブロック "exact ${sample_file_name}" は "${sample_file_path}" と一致しています`
     };
@@ -65,11 +80,11 @@ function handle_diff(textbook_filepath: string, command_args: string[], matched_
   const new_sample_file_name = command_args[2];
   const old_sample_file_path = src_folder + old_sample_file_name;
   const new_sample_file_path = src_folder + new_sample_file_name;
-  const actual_diff = getDiffFromFileNames(old_sample_file_path, new_sample_file_path);
+  const actual_diff = getDiffFromFileNames(old_sample_file_path, new_sample_file_path, command_args.join(" "));
   // 空行に対しての trailing space でテストが落ちるのはしょうもないので、あらかじめ両者から削っておく
   if (trimEndOnAllLines(matched_file_content) !== trimEndOnAllLines(actual_diff)) {
     return {
-      success: false, message: ` MISMATCH FOUND 
+      is_success: false, message: ` MISMATCH FOUND 
 in ${textbook_filepath}
 with the code block labeled "diff ${old_sample_file_name} ${new_sample_file_name}"
 The diff of ${old_sample_file_path} with ${new_sample_file_path} is as follows: \n\`\`\`\n${actual_diff}\`\`\` 
@@ -77,7 +92,7 @@ But the content in the textbook is as follows: \n\`\`\`\n${matched_file_content}
 `};
   } else {
     return {
-      success: true,
+      is_success: true,
       message: ` OK: "${textbook_filepath}" のコードブロック "diff ${old_sample_file_name} ${new_sample_file_name}" は "${old_sample_file_path}" と "${new_sample_file_path}" の diff と一致しています`
     };
   }
@@ -87,7 +102,7 @@ function handle_partial(textbook_filepath: string, command_args: string[], match
   const starting_line_num = Number(command_args[2]);
   const sample_file_name = command_args[1];
   const sample_file_path = src_folder + sample_file_name;
-  const sample_file_content = fs.readFileSync(sample_file_path, { encoding: "utf-8" }).replace(/\r?\n/g, "\n");
+  const sample_file_content = readFileSync(sample_file_path, command_args.join(" ")).replace(/\r?\n/g, "\n");
 
   // 末尾の改行を削って行数を数える
   const 行数 = matched_file_content.trimEnd().split("\n").length;
@@ -98,7 +113,7 @@ function handle_partial(textbook_filepath: string, command_args: string[], match
   // 空行に対しての trailing space でテストが落ちるのはしょうもないので、あらかじめ両者から削っておく
   if (trimEndOnAllLines(partial_content.trimEnd()) !== trimEndOnAllLines(matched_file_content.trimEnd())) {
     return {
-      success: false,
+      is_success: false,
       message: ` MISMATCH FOUND 
           in ${textbook_filepath}
           with the code block labeled "partial ${sample_file_name} ${starting_line_num}"
@@ -107,13 +122,19 @@ function handle_partial(textbook_filepath: string, command_args: string[], match
     }
   } else {
     return {
-      success: true,
+      is_success: true,
       message: ` OK: "${textbook_filepath}" のコードブロック "partial ${sample_file_name} ${starting_line_num}" は "${sample_file_path}" の ${starting_line_num} 行目からの ${行数} 行と一致しています`
     };
   }
 }
 
 export function inspect_codeblock_and_return_message(textbook_filepath: string, config: { src: string }): TestRes[] {
+  if (!fs.existsSync(textbook_filepath)) {
+    return [{
+      is_success: false,
+      message: ` INCORRECT TEXTBOOK FILEPATH "${textbook_filepath}"`
+    }];
+  }
   const textbook_content = fs.readFileSync(textbook_filepath, { encoding: "utf-8" }).replace(/\r?\n/g, "\n");
   const match = [...textbook_content.matchAll(/<!--\s*assert[-_]codeblock\s+(.*?)-->[\n\s]*```(.*?)\n([\s\S]*?)```/gm)]
   if (!match.length) return [];
@@ -121,17 +142,28 @@ export function inspect_codeblock_and_return_message(textbook_filepath: string, 
   console.log(`\n\x1b[34m assert-codeblock: ${textbook_filepath} をチェック中\x1b[0m`);
   return match.map(m => m.slice(1)).map(
     ([command, file_type, matched_file_content]) => {
-      const command_args = command.trim().split(/\s+/);
-      if (command_args[0] === "exact") {
-        return handle_exact(textbook_filepath, command_args, matched_file_content, config.src);
-      } else if (command_args[0] === "diff") {
-        return handle_diff(textbook_filepath, command_args, matched_file_content, config.src);
-      } else if (command_args[0] === "partial") {
-        return handle_partial(textbook_filepath, command_args, matched_file_content, config.src);
-      } else {
-        return {
-          success: false,
-          message: ` assert-codeblock は ${JSON.stringify(command)} というコマンドをサポートしていません`
+      try {
+        const command_args = command.trim().split(/\s+/);
+        if (command_args[0] === "exact") {
+          return handle_exact(textbook_filepath, command_args, matched_file_content, config.src);
+        } else if (command_args[0] === "diff") {
+          return handle_diff(textbook_filepath, command_args, matched_file_content, config.src);
+        } else if (command_args[0] === "partial") {
+          return handle_partial(textbook_filepath, command_args, matched_file_content, config.src);
+        } else {
+          return {
+            is_success: false,
+            message: ` assert-codeblock は ${JSON.stringify(command)} というコマンドをサポートしていません`
+          }
+        }
+      } catch (e) {
+        if (e instanceof WrongFileNameInCommandError) {
+          return {
+            is_success: false,
+            message: e.message
+          };
+        } else {
+          throw e;
         }
       }
     }
