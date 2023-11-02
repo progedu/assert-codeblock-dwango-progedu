15971390d3904088796c1c61a97b49e6fcd000fc
hsjoihs <hirotaka_satou@dwango.co.jp>
2023-11-02 12:40:22 +0900

    Split off files

---
+++ src/util.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+
+export class WrongFileNameInCommandError extends Error {
+  static {
+    this.prototype.name = "WrongFileNameInCommandError";
+  }
+}
+
+export function readFileSync(file_name: string, code_block_label: string): string {
+  if (!fs.existsSync(file_name)) {
+    throw new WrongFileNameInCommandError(` FILE NOT FOUND 
+Cannot find a file "${file_name}" mentioned in the code block labeled "${code_block_label}" `);
+  }
+  return fs.readFileSync(file_name, { encoding: "utf-8" });
+}
+
+export type TestRes = { is_success: Boolean, message: string, additionally?: unknown };
+
+export function trimEndOnAllLines(str: string) {
+  return str.split("\n").map(line => line.trimEnd()).join("\n");
+}
+
+export function FILTER(s: string): string {
+  return trimEndOnAllLines(s.replace(/\r?\n/g, "\n"))
+}
